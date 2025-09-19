677c0fbf58427f9135d4b448a6a997aa3aadfb0c
Keston C. Cole <kestoncole3456@gmail.com>
2025-09-19 10:56:47 -0500

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
     className={`flex items-center justify-between px-6  md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" && "bg-light"}`}>
 
         <Link to='/'>
-            <motion.img whileHover={{scale: 1.05}} src={assets.logo} alt="logo"  className="h-28"/>
+            <motion.img whileHover={{scale: 1.05}} src={assets.logo} alt="logo"  className="h-20"/>
         </Link>
 
         <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${open ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}>
@@ -64,14 +64,19 @@ const Navbar = () => {
                 </Link>
             ))}
 
-            <div className='hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56'>
-                <input type="text" className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" placeholder="Search cars"/>
-                <img src={assets.search_icon} alt="search" />
+            <div className='flex items-center gap-3'>
+                <div className='bg-indigo-600 rounded-full w-10 h-10 flex items-center justify-center'>
+                    <Phone size={20} className='text-white' />
+                </div>
+                <div>
+                    <p className='text-gray-500 text-sm'>Need help?</p>
+                    <p className='font-medium'>+1 876-227-2810</p>
+                </div>
             </div>
 
             <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
                 {isOwner && (
-                    <button onClick={()=> navigate('/owner')} className="cursor-pointer">Dashboard</button>
+                    <button onClick={()=> navigate('/owner')} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">Dashboard</button>
                 )}
 
                 <button onClick={()=> {user ? logout() : setShowLogin(true)}} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">{user ? 'Logout' : 'Login'}</button>

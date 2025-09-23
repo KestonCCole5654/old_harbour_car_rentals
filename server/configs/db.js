a0809a7078625c5f2f0cdb5efb05f65cea25aa73
Keston C. Cole <kestoncole3456@gmail.com>
2025-09-23 18:29:36 -0500

    Shelmur Car Rentals Limited V2.3

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,15 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/car-rental`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/car-rental`, {
+            bufferMaxEntries: 0,
+            serverSelectionTimeoutMS: 30000,
+            socketTimeoutMS: 45000,
+            connectTimeoutMS: 30000,
+            maxPoolSize: 10,
+            minPoolSize: 5,
+            maxIdleTimeMS: 30000
+        })
     } catch (error) {
         console.log(error.message);
     }
